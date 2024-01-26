caa991496da5b786e2d5d817c35dbc81da82f9f0
Mostefa Ben Naceur <bennaceurmostefa@gmail.com>
2024-01-27 00:27:28 +0100

    Update conv_fact
    16 decimal places

--- Kilom2miles.py
+++ Kilom2miles.py
@@ -2,8 +2,8 @@
 kilometers = float(input("Enter value in kilometers: "))
 
 # conversion factor
-conv_fac = 0.621371
+CONV_FACT = 0.6213711234567890
 
 # calculate miles
-miles = kilometers * conv_fac
+miles = kilometers * CONV_FACT
 print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
