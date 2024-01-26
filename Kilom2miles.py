f67912a35a5a6af83d3fdb800220164f777ef080
Mostefa Ben Naceur <bennaceurmostefa@gmail.com>
2024-01-27 00:18:29 +0100

    made conv_fac
    made conv_fac 0.621371456921

--- Kilom2miles.py
+++ Kilom2miles.py
@@ -1,8 +1,7 @@
 # Taking kilometers input from the user
 kilometers = float(input("Enter value in kilometers: "))
 
-# conversion factor
-conv_fac = 0.621371
+conv_fac = 0.621371456921;# conversion factor
 
 # calculate miles
 miles = kilometers * conv_fac
